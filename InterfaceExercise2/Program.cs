67abc71126fd705b8f4d9835d052bace93eeff21
Damion Russell <domino.russell@gmail.com>
2022-07-22 13:08:11 -0400

    initial commit

---
+++ InterfaceExercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What kind of interface to store your data? List, SQL, or Mongo?");
+            string userInput = Console.ReadLine();
+            var myData = DataAccessFactory.GetDataAcessType(userInput);
+            myData.LoadData();
+            myData.SaveData();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InterfaceExercise2/SQLDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise2
+{
+    public class SQLDataAccess : IDataAccess
+    {
+        public void LoadData()
+        {
+            Console.WriteLine($"I am reading data from {this}");
+        }
+        public void SaveData()
+        {
+            Console.WriteLine($"Data has been saved to {this}");
+        }
+    }
+}
